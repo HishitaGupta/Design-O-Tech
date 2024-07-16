0ccd989effe6dbf1c3b332c51d5987172d25f7d6
Hishita Gupta <159264060+HishitaGupta@users.noreply.github.com>
2024-07-16 13:11:03 +0530

    made charter immigration website

---
+++ Immigration/script.js
@@ -0,0 +1,26 @@
+const sr= ScrollReveal({
+    origin:'top',
+    distance: '500px',
+    duration: 2500 ,
+    delay: 400,
+    // reset:true,//animations repeat
+    
+
+})
+sr.reveal(`#img1,#img3`,{origin:'left',opacity:1})
+sr.reveal(`#img2,#img4`,{origin:'right',opacity:1})
+// sr.reveal(`#hero-h1`,{origin:'top',opacity:1})
+// sr.reveal(`#hero-h6`,{origin:'bottom',opacity:1})
+
+const sr2= ScrollReveal({
+    origin:'top',
+    distance: '50px',
+    duration: 2500 ,
+    delay: 400,
+    // reset:true,//animations repeat
+    
+
+})
+sr2.reveal(`#hero-h1`,{origin:'top',opacity:1})
+sr2.reveal(`#hero-h6, #contact-btn`,{origin:'bottom',opacity:1})
+
